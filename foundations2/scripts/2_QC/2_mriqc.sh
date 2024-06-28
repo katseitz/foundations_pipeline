17f54458b684ac0ac6d04472e0e6799ab4b36afa
Kat Seitz <katharina.seitz@northwestern.edu>
2024-06-28 10:49:44 -0500

    syncing

--- foundations2/scripts/2_QC/2_mriqc.sh
+++ foundations2/scripts/2_QC/2_mriqc.sh
@@ -3,7 +3,7 @@
 #SBATCH -A p31833
 #SBATCH -p short
 #SBATCH -t 04:00:00
-#SBATCH --mem=20G
+#SBATCH --mem=28G
 #SBATCH -J qc-found
 #SBATCH	--mail-type=END,FAIL
 #SBATCH	--mail-user=katharinaseitz@northwestern.edu
